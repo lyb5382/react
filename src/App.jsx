9a1a0bf2dde16b24c927df29f676a7fc6bbd809e
lyb5382 <lyb538202@gmail.com>
2025-08-06 14:04:53 +0900

    v

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,11 @@
 import './App.css'
-import Button from './componentes/Button'
-import Input from './componentes/Input'
+import Counter from './componentes/Counter'
 
 function App() {
 
   return (
     <div>
-      <Input inputValue={'hello React'} title={'title'} placeholder={'input'}/>
-      <h1>hello React!</h1>
-      <Button text={'mail'} color={'skyblue'} />
-      <Button text={'cafe'} color={'blue'} />
-      <Button text={'blog'} color={'purple'} />
-      <Button text={'normal'} />
-      <Button text={'child'} color={'darkcyan'} >
-        <strong>importance</strong>
-      </Button>
+      <Counter />
     </div>
   )
 }
---
+++ src/componentes/Counter.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react'
+
+const Counter = () => {
+    const [count, setCount] = useState(0)
+    const decrementCount = () => {
+        setCount(count - 1)
+    }
+    const incrementCount = () => {
+        setCount(count + 1)
+    }
+    const resetCount = () => {
+        setCount(0)
+    }
+    return (
+        <div>
+            <h1>{count}</h1>
+            <button onClick={decrementCount}>-</button>
+            <button onClick={resetCount}>reset</button>
+            <button onClick={incrementCount}>+</button>
+        </div>
+    )
+}
+
+export default Counter
